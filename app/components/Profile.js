12b4e806c00896fd6944ea53018c64f059c3e74d
Victor Wibisono <vwibisono@dius.com.au>
2016-01-07 12:15:36 +1100

    Some es6 refactoring.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,44 +5,45 @@ var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
 var Firebase = require('firebase');
-var helpers = require('../utils/helpers');
+
+import getGithubInfo from '../utils/helpers';
 
 var Profile = React.createClass({
   mixins: [ReactFireMixin],
-  getInitialState: function(){
+  getInitialState: function () {
     return {
       notes: [],
       bio: {},
       repos: []
     }
   },
-  componentDidMount: function() {
+  componentDidMount: function () {
     this.ref = new Firebase('https://radiant-heat-7824.firebaseio.com/');
     this.init(this.props.params.username);
   },
-  componentWillReceiveProps: function(nextProps){
+  componentWillReceiveProps: function (nextProps) {
     this.unbind('notes');
     this.init(nextProps.params.username);
   },
-  componentWillUnmount: function() {
+  componentWillUnmount: function () {
     this.unbind('notes');
   },
-  init: function(username){
+  init: function (username) {
     var childRef = this.ref.child(username);
     this.bindAsArray(childRef, 'notes');
 
-    helpers.getGithubInfo(username)
-      .then(function(data){
+    getGithubInfo(username)
+      .then(function (data) {
         this.setState({
           bio: data.bio,
           repos: data.repos
         });
       }.bind(this));
   },
-  handleAddNote: function(newNote){
+  handleAddNote: function (newNote) {
     this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote);
   },
-  render: function(){
+  render: function () {
     return (
       <div className="row">
         <div className="col-md-4">
